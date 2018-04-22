5d7256ab2f724dc659c981009daac581591a1559
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2018-04-22 18:05:14 +0700

    Fix typo in test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 EMACS=emacs
 
-EMACS -batch -l ert \
-      -L $PWD \
-      -l test.el \
-      -f ert-run-tests-batch-and-exit
+$EMACS -batch -l ert \
+       -L $PWD \
+       -l test.el \
+       -f ert-run-tests-batch-and-exit
